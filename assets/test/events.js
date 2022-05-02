60d810077084da912a6dca5495ced80e5d509f37
Bittorn <48916178+Bittorn@users.noreply.github.com>
2022-05-02 15:27:54 +0200

    JSON fix

--- assets/test/events.js
+++ assets/test/events.js
@@ -1,4 +1,4 @@
-{
+export default {
     "events" : [
         {
             "eventname":"event name",
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 // Main script content
 
-import eventsJSON from './assets/test/events.json'; 
+import eventsJSON from './assets/test/events.js'; 
 document.getElementById("text").innerHTML = eventsJSON;
