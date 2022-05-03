3ce438e2ea7297e4e43a851182eb1fa2d0d85854
Bittorn <48916178+Bittorn@users.noreply.github.com>
2022-05-03 11:08:57 +0200

    JSON hotfix

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+// Event stuff
 const eventHeader = document.getElementById("event-header").innerHTML;
 const eventText = document.getElementById("event-text").innerHTML;
 const eventButtons = document.getElementById("event-options")
 
 import eventsJSON from './assets/test/events.js';
 
-eventHeader = eventsJSON.events[0].name;
-eventText = eventsJSON.events[0].text;
-
-for (const x of eventsJSON.events[0].options) {
-    const btn = document.createElement("button");
-    btn.innerHTML = x.text;
-    eventButtons.appendChild(btn);
+function loadEvent(event) {
+    eventHeader = eventsJSON.events[event].name;
+    eventText = eventsJSON.events[event].text;
+    
+    for (const x of eventsJSON.events[event].options) {
+        const btn = document.createElement("button");
+        btn.innerHTML = x.text;
+        btn.onclick = loadEvent(x.destination);
+        eventButtons.appendChild(btn);
+    }
 }
+
+document.getElementsByClassName("container").onload = loadEvent(0);
