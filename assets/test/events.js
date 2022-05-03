7ebf19b688c384bd9e0faba6e78af71720537e6c
Bittorn <48916178+Bittorn@users.noreply.github.com>
2022-05-03 09:55:57 +0200

    Lesgoo

--- assets/test/events.js
+++ assets/test/events.js
@@ -1,19 +1,42 @@
 export default {
     "events" : [
         {
-            "eventname":"event name",
-            "eventbody":"event details",
-            "eventoptions" : [
+            "id" : 1,
+            "name" : "The First Event",
+            "text" : "Some event details here",
+            "options" : [
                 {
-                    "text":"option text 1",
-                    "linksto":"link"
+                    "text" : "Go to 2",
+                    "destination" : 2
                 },
                 {
-                    "text":"option text 2",
-                    "linksto":"link 2"
+                    "text" : "Go to 3",
+                    "destination" : 3
                 }
             ]
             
+        },
+        {
+            "id" : 2,
+            "name" : "The Second Event",
+            "text" : "Some more event details here",
+            "options" : [
+                {
+                    "text" : "Return",
+                    "destination" : 1
+                }
+            ]
+        },
+        {
+            "id" : 3,
+            "name" : "The Third Event",
+            "text" : "Some more more event details here",
+            "options" : [
+                {
+                    "text" : "Return",
+                    "destination" : 1
+                }
+            ]
         }
     ]
 }
