55a3acf4bfc02a70b2f68ba0de159f1c4b2990e7
Bittorn <48916178+Bittorn@users.noreply.github.com>
2022-05-03 10:31:22 +0200

    JSON parse hotfix

--- main.js
+++ main.js
@@ -2,25 +2,21 @@
 
 const eventHeader = document.getElementById("event-header");
 const eventText = document.getElementById("event-text");
-import eventsJS from './assets/test/events.js';
 
 try {
-    const eventsJSON = JSON.parse(eventsJS);
+    import eventsJS from './assets/test/events.js';
+
+    try {
+        eventHeader.innerHTML = eventsJSON.events.name;
+    } catch (error) {
+        eventHeader.innerHTML = "Event name error";
+    }
+    try {
+        eventText.innerHTML = eventsJSON.events.text;
+    } catch (error) {
+        eventText.innerHTML = "Event text error";
+    }
 } catch (error) {
     eventHeader.innerHTML = "Event load error"
     eventText.innerHTML = error.message;
 }
-
-/*try {
-    eventHeader.innerHTML = eventsJSON.events.name;
-} catch (error) {
-    eventHeader.innerHTML = "Event name error";
-}
-
-try {
-    eventText.innerHTML = eventsJSON.events.text;
-} catch (error) {
-    eventText.innerHTML = "Event text error";
-}*/
-
-//eventText.innerHTML = eventsJSON;
