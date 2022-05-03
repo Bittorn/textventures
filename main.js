26a90d7be0ed8bf9bf94138decc8ea7ae2a68da4
Bittorn <48916178+Bittorn@users.noreply.github.com>
2022-05-03 10:35:08 +0200

    Error fix

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-// Main script content
-
 const eventHeader = document.getElementById("event-header");
 const eventText = document.getElementById("event-text");
 
-try {
-    import eventsJS from './assets/test/events.js';
+import eventsJS from './assets/test/events.js';
 
-    try {
-        eventHeader.innerHTML = eventsJSON.events.name;
-    } catch (error) {
-        eventHeader.innerHTML = "Event name error";
-    }
-    try {
-        eventText.innerHTML = eventsJSON.events.text;
-    } catch (error) {
-        eventText.innerHTML = "Event text error";
-    }
+try {
+    eventHeader.innerHTML = eventsJSON.events.name;
 } catch (error) {
-    eventHeader.innerHTML = "Event load error"
+    eventHeader.innerHTML = error.message;
+}
+
+try {
+    eventText.innerHTML = eventsJSON.events.text;
+} catch (error) {
     eventText.innerHTML = error.message;
 }
