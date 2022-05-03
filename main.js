732d0c34defbcac3fafddad889793b8312169e68
Bittorn <48916178+Bittorn@users.noreply.github.com>
2022-05-03 10:17:42 +0200

    JSON parsing tests

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 // Main script content
 
-import eventsJSON from './assets/test/events.js'; 
-document.getElementById("event-text").innerHTML = JSON.stringify(eventsJSON);
+const eventHeader = document.getElementById("event-header");
+const eventText = document.getElementById("event-text");
+import eventsJS from './assets/test/events.js';
+
+try {
+    const eventsJSON = JSON.parse(eventsJS);
+} catch (error) {
+    eventHeader.innerHTML = "Event load error"
+    eventText.innerHTML = error.message;
+}
+
+try {
+    eventHeader.innerHTML = eventsJSON.events.name;
+} catch (error) {
+    eventHeader.innerHTML = "Event name error";
+}
+
+try {
+    eventText.innerHTML = eventsJSON.events.text;
+} catch (error) {
+    eventText.innerHTML = "Event text error";
+}
