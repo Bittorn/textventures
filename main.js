e2d6dfa41cf061ede5a04c3c37279aa8ba4d8bc6
Bittorn <48916178+Bittorn@users.noreply.github.com>
2022-05-03 11:47:11 +0200

    JSON parsing test 3

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 // Event stuff
-let eventHeader = document.getElementById("event-header").innerHTML;
-let eventText = document.getElementById("event-text").innerHTML;
+let eventHeader = document.getElementById("event-header");
+let eventText = document.getElementById("event-text");
 let eventButtons = document.getElementById("event-options")
 
 import eventsJSON from './assets/test/events.js';
 
 function loadEvent(event) {
+    eventButtons.innerHTML = "";
+    
     console.log("container loaded");
-    eventHeader = eventsJSON.events[event].name;
-    eventText = eventsJSON.events[event].text;
+    eventHeader.innerHTML = eventsJSON.events[event].name;
+    eventText.innerHTML = eventsJSON.events[event].text;
     
     for (const x of eventsJSON.events[event].options) {
         const btn = document.createElement("button");
         btn.innerHTML = x.text;
         btn.onclick = () => loadEvent(x.destination);
+        btn.classList.add("btn");
         eventButtons.appendChild(btn);
     }
 }
 
-console.log(eventsJSON);
-console.log(eventsJSON.events);
-console.log(eventsJSON.events[0]);
-console.log(eventsJSON.events[0].name);
-document.getElementsByClassName("container").onload = loadEvent(0);
+window.onload = () => loadEvent(0);
